708c839d2fbccec4454b6e455193cb66373176dd
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2024-12-11 18:33:01 +0545

    Post method in API view decorators

--- Home/views.py
+++ Home/views.py
@@ -2,11 +2,16 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 # Create your views here.
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def index(request):
     courses = {
         'course_name': 'Python',
         'learn': ['flask', 'Django', 'FastApi'],
         'course_provider': 'Scaler'
     }
-    return Response(courses)
+    if request.method == 'GET':
+        print('you hit get method.')
+        return Response(courses)
+    elif request.method == 'POST':
+        print('you hit post method.')
+        return Response(courses)
