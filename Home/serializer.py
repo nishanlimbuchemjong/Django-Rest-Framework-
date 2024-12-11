20b580f2dbd57dcdba1c4dc61d1748803a5c6d26
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2024-12-11 20:21:16 +0545

    added SerializerMethodField() to view more color_info details defining its method get_color_inof()

--- Home/serializer.py
+++ Home/serializer.py
@@ -8,10 +8,16 @@ class ColorSerializer(serializers.ModelSerializer):
 
 class PeopleSerializer(serializers.ModelSerializer):
     color = ColorSerializer()
+    color_info = serializers.SerializerMethodField()
+
     class Meta:
         model = Person
         fields = '__all__'
         # depth = 1
+
+    def get_color_info(self, obj):
+        color_obj = Color.objects.get(id=obj.color.id)
+        return {'color_name': color_obj.color_name, 'hex_code': '#000'}
 
     def validate(self, data):
         special_characters = "!@#%^&*()-_+=,<>?/"
